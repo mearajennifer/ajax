570efaa3966555ffb793abd7ce6f1610a0c7ef35
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-13 17:04:23 -0700

    Added part 3

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -34,15 +34,25 @@ function showWeather(evt) {
 $("#weather-form").on('submit', showWeather);
 
 
-
-
 // PART 3: ORDER MELONS
+function showOrderResult(result) {
+	const code = `
+		STATUS: ${result.code}
+		${result.msg}
+	`;
+	alert(code);
+}
 
 function orderMelons(evt) {
     evt.preventDefault();
 
+    let formInputs = {
+    	'melon_type': $("#melon-type-field").val(),
+    	'qty': $("#qty-field").val(),
+    };
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    $.post('/order-melons.json', formInputs, showOrderResult);
 }
 
 $("#order-form").on('submit', orderMelons);
